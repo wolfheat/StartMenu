40e8dad25c5adda73f0c2cdf59dffc6a2735ea42
wolfheat <mihe82@gmail.com>
2024-01-31 23:26:26 +0100

    Percent Fix

--- Assets/Scripts/SettingsController.cs
+++ Assets/Scripts/SettingsController.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,9 +7,28 @@ public class SettingsController : MonoBehaviour
     [SerializeField] Slider master;
     [SerializeField] Slider music;
     [SerializeField] Slider sfx;
+    [SerializeField] TextMeshProUGUI masterPercent;
+    [SerializeField] TextMeshProUGUI musicPercent;
+    [SerializeField] TextMeshProUGUI sfxPercent;
+
+    private void OnEnable()
+    {
+        UpdateSoundPercent();
+    }
+
+    public void UpdateSoundPercent()
+    {
+        // Update percent
+        masterPercent.text = (master.value*100).ToString("F0");
+        musicPercent.text = (music.value*100).ToString("F0");
+        sfxPercent.text = (sfx.value*100).ToString("F0");
+    }
     public void UpdateSound()
     {
         SoundMaster.Instance.UpdateVolume(master.value,music.value, sfx.value);
+
+        UpdateSoundPercent();
+
     } 
     public void SFXSliderChange()
     {
